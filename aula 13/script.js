6b4877a7c8c5e306b581296fc3006e15669ed1b1
Sauerrr <sauervitor12@gmail.com>
2023-09-21 14:48:58 -0300

    aula 13

--- aula 13/script.js
+++ aula 13/script.js
@@ -20,3 +20,16 @@ function divisao (n1, n2){
 function somar(){
      document.getElementById("resultado").value = soma(Number(document.getElementById("n1").value), Number(document.getElementById("n2").value));
 }
+
+function subtrair(){
+     document.getElementById("resultado").value = subtracao(Number(document.getElementById("n1").value), Number(document.getElementById("n2").value));
+}
+
+function multiplicar(){
+     document.getElementById("resultado").value = multiplicacao(Number(document.getElementById("n1").value), Number(document.getElementById("n2").value));
+}
+
+function dividir(){
+     document.getElementById("resultado").value = divisao(Number(document.getElementById("n1").value), Number(document.getElementById("n2").value));
+}
+
